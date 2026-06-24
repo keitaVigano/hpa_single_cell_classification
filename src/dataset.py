79f1927f0a139a6c7041118af84e0b7f4f812ac4
keita jacopo vigano <keitainter@gmail.com>
2026-06-24 22:27:58 +0200

    corrrected circular import

--- src/dataset.py
+++ src/dataset.py
@@ -2,8 +2,8 @@ from torch.utils.data import Dataset
 import torch
 import numpy as np
 import cv2
+import os
 
-from src.utils.common import load_img_channel, load_img
 
 class HPADataset(Dataset):
     def __init__(self, image_ids, label_map, path_data, path_masks, img_size=224):
@@ -16,7 +16,7 @@ class HPADataset(Dataset):
         for image_id in image_ids:
             if image_id not in label_map:
                 continue
-            mask = load_img_channel(image_id, path_masks)
+            mask = self.load_img_channel(image_id, path_masks)
             if mask is None:
                 continue
             cell_ids = np.unique(mask)
@@ -32,15 +32,15 @@ class HPADataset(Dataset):
         rows, cols = np.where(cell_mask == cell_id)
         y_min, y_max = rows.min(), rows.max()
         x_min, x_max = cols.min(), cols.max()
-        crop = img[y_min:y_max, x_min:x_max, :]
+        crop = img[y_min:y_max + 1, x_min:x_max + 1, :]
         crop = cv2.resize(crop, (img_size, img_size))
         return crop
 
     def __getitem__(self, idx):
         image_id, cell_id = self.samples[idx]
 
-        img = load_img(image_id, self.path_data)
-        mask = load_img_channel(image_id, self.path_masks)
+        img = self.load_img(image_id, self.path_data)
+        mask = self.load_img_channel(image_id, self.path_masks)
 
         crop = HPADataset.get_crops(img, mask, cell_id, self.img_size)
 
@@ -50,3 +50,25 @@ class HPADataset(Dataset):
         label = torch.from_numpy(self.label_map[image_id])
 
         return crop, label
+
+    @staticmethod
+    def load_img(image_id: str, path: str) -> np.ndarray:
+        red = HPADataset.load_img_channel(f"{image_id}_red", path)
+        green = HPADataset.load_img_channel(f"{image_id}_green", path)
+        blue = HPADataset.load_img_channel(f"{image_id}_blue", path)
+        yellow = HPADataset.load_img_channel(f"{image_id}_yellow", path)
+        if red is None or blue is None or yellow is None or green is None:
+            raise FileNotFoundError(f"Missing one or more channel files for image_id={image_id} in {path}")
+        return np.stack([red, green, blue, yellow], axis=-1)  # (H, W, 4)
+
+    @staticmethod
+    def norm(img: np.ndarray) -> np.ndarray:
+        img_float = img.astype(np.float32)
+        return (img_float - img_float.min()) / (img_float.max() - img_float.min() + 1e-8)
+
+    @staticmethod
+    def load_img_channel(image_id: str, path: str) -> np.ndarray | None:
+        mask_path: str = f"{path}/{image_id}.png"
+        if not os.path.exists(mask_path):
+            return None
+        return cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
--- src/utils/common.py
+++ src/utils/common.py
@@ -11,69 +11,6 @@ import pandas as pd
 import torch.nn as nn
 import wandb
 
-def load_img(image_id: str, path: str) -> np.ndarray:
-    red = load_img_channel(f"{image_id}_red", path)
-    green = load_img_channel(f"{image_id}_green", path)
-    blue = load_img_channel(f"{image_id}_blue", path)
-    yellow = load_img_channel(f"{image_id}_yellow", path)
-    if red is None or blue is None or yellow is None or green is None:
-        raise FileNotFoundError(f"Missing one or more channel files for image_id={image_id} in {path}")
-    return np.stack([red, green, blue, yellow], axis=-1)  # (H, W, 4)
-
-
-def norm(img: np.ndarray) -> np.ndarray:
-    img_float = img.astype(np.float32)
-    return (img_float - img_float.min()) / (img_float.max() - img_float.min() + 1e-8)
-
-
-def load_img_channel(image_id: str, path: str) -> np.ndarray | None:
-    mask_path: str = f"{path}/{image_id}.png"
-    if not os.path.exists(mask_path):
-        return None
-    return cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
-
-
-def plot_img(image_ids: set[str], path: str) -> None:
-    sample_ids: list[str] = list(image_ids)[:10]
-
-    fig, axes = plt.subplots(2, 5, figsize=(20, 8))
-    axes = axes.flatten()
-
-    for i, image_id in enumerate(sample_ids):
-        img = load_img(image_id, path)
-        rgb = np.stack([norm(img[..., 0]), norm(img[..., 1]), norm(img[..., 2])], axis=-1)
-        axes[i].imshow(rgb)
-        axes[i].axis("off")
-        axes[i].set_title(image_id[:8], fontsize=8)
-
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_img_with_mask(image_ids: set[str], path_data: str, path_mask: str) -> None:
-
-    sample_ids = list(image_ids)[:10]
-
-    fig, axes = plt.subplots(2, 5, figsize=(20, 8))
-    axes = axes.flatten()
-
-    for i, image_id in enumerate(sample_ids):
-        img = load_img(image_id, path_data)
-        rgb = np.stack([norm(img[..., 0]), norm(img[..., 1]), norm(img[..., 2])], axis=-1)
-
-        mask = load_img_channel(image_id, path_mask)
-
-        axes[i].imshow(rgb)
-
-        if mask is not None:
-            axes[i].imshow(mask, cmap='tab20', alpha=0.4, interpolation='nearest')
-
-        axes[i].axis('off')
-        axes[i].set_title(image_id[:8], fontsize=8)
-
-    plt.tight_layout()
-    plt.show()
-
 
 def label_to_vector(label_str: str) -> np.ndarray:
     vector = np.zeros(19, dtype=np.float32)
